afca3e7f2460fd392de0ef1f14affbf2e6f7e600
cbnits-144 <shubhamshrm1996@gmail.com>
2021-09-27 13:05:26 +0530

    added

--- demo.py
+++ demo.py
@@ -1,3 +1,6 @@
 def ab():
     d = {"name":"Shubham" , "age" : 26}
     assert d["age"] == 14 ,"test case failed"
+def cd():
+    d = {"name":"Shubham" , "age" : 26}
+    assert d["age"] == 26 ,"test case failed"
