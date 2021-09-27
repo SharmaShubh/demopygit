d8944c33a4019d907a3a16ec884c41146f55f966
cbnits-144 <shubhamshrm1996@gmail.com>
2021-09-27 15:24:17 +0530

    added

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 def ab():
-    d = {"name":"Shubham" , "age" : 26}
+    d = {"id":"1","name":"Shubham" , "age" : 26}
+    print(d["id"])
     assert d["age"] == 26 ,"test case failed"
 def cd():
-    d = {"name":"Shubham" , "age" : 26}
+    d = {"id":"2","name":"Shubham" , "age" : 26}
+    print(d["id"])
     assert d["age"] == 26 ,"test case failed"
